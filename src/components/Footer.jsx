45b8172c958478969aac86d6d953742dd4c215dd
jakebarry <168513840+jakebarry@users.noreply.github.com>
2024-09-18 21:55:55 +1000

    Embed nasa link to title.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,7 +9,8 @@ const Footer = (props) => {
             <div className='bgGradient'></div>
             <div>
                 <h1>APOD Project</h1>
-                <h2>{data?.title}</h2>
+                <h2><a href="https://apod.nasa.gov/apod/">{data?.title}</a></h2>
+                {/* <h2>{data?.title}</h2> */}
             </div>
             <button onClick={handleToggleModal}>
                 <i className="fa-solid fa-circle-info"></i>
